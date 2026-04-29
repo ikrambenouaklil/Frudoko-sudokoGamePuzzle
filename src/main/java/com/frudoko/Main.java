0bb6ae66a39b95f4eb63208fa8eacea7b91a4841
ikrambenouaklil <ikrambn2002@gmail.com>
2026-04-29 04:42:59 +0100

    fix packaging to jar

--- src/main/java/com/frudoko/Main.java
+++ src/main/java/com/frudoko/Main.java
@@ -2,7 +2,12 @@ package com.frudoko;
 
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.Context;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
 import java.io.File;
+import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -13,8 +18,28 @@ public class Main {
         tomcat.setPort(Integer.parseInt(port));
         tomcat.getConnector();
 
-        String webappDir = new File("src/main/webapp").getAbsolutePath();
-        Context ctx = tomcat.addWebapp("", webappDir);
+        // نشوف وين الـ JAR موجود
+        String jarDir;
+        try {
+            jarDir = new File(Main.class.getProtectionDomain()
+                    .getCodeSource().getLocation().toURI()).getParent();
+        } catch (URISyntaxException e) {
+            jarDir = "target";
+        }
+
+        // نستخدم مجلد مؤقت كـ docBase
+        File docBase = new File(System.getProperty("java.io.tmpdir"));
+        Context ctx = tomcat.addWebapp("", docBase.getAbsolutePath());
+
+        // نحمّل الـ resources من داخل الـ JAR
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(
+                resources,
+                "/",
+                new File(jarDir).getAbsolutePath(),
+                "/"
+        ));
+        ctx.setResources(resources);
 
         tomcat.start();
         tomcat.getServer().await();
